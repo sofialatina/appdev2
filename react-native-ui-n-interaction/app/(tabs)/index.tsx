390d21b484cfffd5edb3eef226bc76df9f92b48b
Sofia Latina <sofiaisabellatina@gmail.com>
2026-02-13 15:18:14 +0800

    Done with flex dimensions section

--- react-native-ui-n-interaction/app/(tabs)/index.tsx
+++ react-native-ui-n-interaction/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import StyleExample from 'D:/Latte/GIthub/appdev2/react-native-ui-n-interaction/components/FixedDimensions';
+import StyleExample from 'D:/Latte/GIthub/appdev2/react-native-ui-n-interaction/components/FlexDimensions';
 
 export default function App() {
   return <StyleExample />;
