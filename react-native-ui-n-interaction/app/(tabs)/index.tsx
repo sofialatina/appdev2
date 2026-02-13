d2cce6c5cddcbf249a661b874687c5eb2af45565
Sofia Latina <sofiaisabellatina@gmail.com>
2026-02-13 15:16:23 +0800

    Done with fixed dimensions section

--- react-native-ui-n-interaction/app/(tabs)/index.tsx
+++ react-native-ui-n-interaction/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import StyleExample from 'D:/Latte/GIthub/appdev2/react-native-ui-n-interaction/components/StyleExample';
+import StyleExample from 'D:/Latte/GIthub/appdev2/react-native-ui-n-interaction/components/FixedDimensions';
 
 export default function App() {
   return <StyleExample />;
