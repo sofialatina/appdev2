d2f63b48633233f782be92cc877563b46a1b11c3
Sofia Latina <sofiaisabellatina@gmail.com>
2026-04-17 00:26:45 +0800

    Done with configuring the navigator

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -9,9 +10,20 @@ function HomeScreen() {
     </View>
   );
 }
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
 const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
   screens: {
     Home: HomeScreen,
+    Details: DetailsScreen,
   },
 });
 const Navigation = createStaticNavigation(RootStack);
